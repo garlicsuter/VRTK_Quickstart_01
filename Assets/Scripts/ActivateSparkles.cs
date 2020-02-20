dce689a5e5937c6bdbbce48ee252a0b89c92ddeb
garlicsuter <markasuter@gmail.com>
2020-02-19 19:49:24 -0500

    ready!

---
+++ Assets/Scripts/ActivateSparkles.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateSparkles : MonoBehaviour
+{
+    public GameObject Fireworks;
+    public ParticleSystem ps;
+    
+
+    private void Start()
+    {
+        Fireworks = GameObject.Find("Rocket");
+        ps = Fireworks.GetComponent<ParticleSystem>();
+        ps.Stop();
+    }
+
+    public void Sparkles()
+    {
+        Debug.Log("It was snapped");
+        ps.Play();
+    }
+
+    public void NoSparkles()
+    {
+        Debug.Log("It left.");
+        ps.Stop();
+    }
+
+    
+}
---
+++ Assets/Scripts/MicDrop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicDrop : MonoBehaviour
+{
+    public GameObject MicSpawnLocation;
+    public GameObject Mic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MicDropper()
+    {
+        Debug.Log("Drop Mic When Ready.");
+        Instantiate(Mic, MicSpawnLocation.transform.position, MicSpawnLocation.transform.rotation);
+    }
+}
